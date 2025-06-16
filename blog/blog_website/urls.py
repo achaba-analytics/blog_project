34c95b5bcfdfe76a589a0e88d258e13c7ea016a4
achaba-analytics <achaba.analytics@gmail.com>
2025-06-16 12:25:53 +0300

    My Second Commit

---
+++ blog/blog_website/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+#from . import views
+from .views import HomeView, ArticleView, AddPostView, UpdateView, DeleteView, AddCategoryView, CategoryView, CategoryListView, LikeView
+
+urlpatterns = [
+    #path('', views.home, name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>',ArticleView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('article/edit/<int:pk>', UpdateView.as_view(), name='update'),
+    path('article/<int:pk>/delete', DeleteView.as_view(), name='delete'),
+    path('category/<str:cats>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category_list'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+]
---
+++ blog/blog_website/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Category
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+
+#def home(request):
+ # template = loader.get_template('home.html')
+#  return HttpResponse(template.render())
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    ordering = ['-pub_date']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
+class ArticleView(DetailView):
+    model = Post
+    template_name = 'articles.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleView, self).get_context_data(*args, **kwargs)
+        
+        test = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = test.total_likes()
+        context['cat_menu'] = cat_menu
+        context['total_likes'] = total_likes
+        return context
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(AddPostView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+    
+
+class AddCategoryView(CreateView):
+    model = Category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(AddCategoryView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats.replace('-', '%20'))
+    return render(request, 'categories.html', {'cats':cats, 'category_posts':category_posts})
+
+class UpdateView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update.html'
+    #fields = ['title', 'title_tag', 'body']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(UpdateView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
+class DeleteView(DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(DeleteView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+    
+
+class AddPostView(CreateView):
+
+        model = Post
+        form_class = PostForm
+        template_name = 'add_post.html'
+
+def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+    
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+
